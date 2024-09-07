f5f8eddc4ab212ff458c7c607c1aff3bc635a65b
Sergey Brazhnikov <serjak.2012@gmail.com>
2024-09-08 05:23:00 +0600

    step 2: create server on index.ts create api configs on config.ts create interfaces userfield usermetods usermodel tasks on types.ts

---
+++ todoApi/types.ts
@@ -0,0 +1,23 @@
+import mongoose, {Model} from 'mongoose';
+
+export interface UserFields {
+  username: string;
+  password: string;
+  token: string;
+}
+
+export interface UserMethods {
+  checkPassword(password: string): Promise<boolean>;
+
+  generateToken(): void;
+}
+
+export type UserModel = Model<UserFields, {}, UserMethods>;
+
+export interface Tasks {
+  user: string;
+  title: string;
+  description: string;
+  status: string;
+}
+
